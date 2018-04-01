7c0d71545c6fbd537aa0a8ec9976848a3eddd441
Simona Cotin <simona.cotin@gmail.com>
2018-04-01 11:44:37 +0100

    Test listings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+import { ListingsComponent } from './listings-list/listings/listings.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: ListingsComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' }
 ];
 
